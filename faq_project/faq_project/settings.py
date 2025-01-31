14507ccaa9138c25a500bc991d01e9383c7ec63f
hellspwan679 <mehulsharma4786@gmail.com>
2025-02-01 04:39:21 +0530

    fix: added whitenoise for static site serving in aws
    Signed-off-by: hellspwan679 <mehulsharma4786@gmail.com>

--- faq_project/faq_project/settings.py
+++ faq_project/faq_project/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'faq_project.urls'
@@ -131,6 +132,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = 'static/'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
