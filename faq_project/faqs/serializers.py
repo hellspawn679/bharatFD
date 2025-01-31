b35b837a2634361f546d7d9f071c50d10cd770e4
hellspwan679 <mehulsharma4786@gmail.com>
2025-01-31 22:21:57 +0530

    fix : dynamic language fetch view and seralization
    Signed-off-by: hellspwan679 <mehulsharma4786@gmail.com>

--- faq_project/faqs/serializers.py
+++ faq_project/faqs/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import FAQ
 
-
 class FAQSerializer(serializers.ModelSerializer):
+    # Define fields for serialized FAQ data
+    question = serializers.CharField()
+    answer = serializers.CharField()
+
     class Meta:
         model = FAQ
         fields = ['id', 'question', 'answer']
--- faq_project/faqs/views.py
+++ faq_project/faqs/views.py
@@ -3,15 +3,23 @@ from rest_framework.decorators import api_view
 from .models import FAQ
 from .serializers import FAQSerializer
 
-
 @api_view(['GET'])
 def get_faqs(request):
     lang = request.GET.get('lang', 'en')
     faqs = FAQ.objects.all()
 
-    # Modify queryset with translations
-    for faq in faqs:
-        faq.question = faq.get_translation(lang)
+    # Create a list of translated FAQ data
+    faq_data = []
 
-    serializer = FAQSerializer(faqs, many=True)
+    for faq in faqs:
+        translated_data = faq.get_translation(lang)
+        # Append translated data to the faq_data list
+        faq_data.append({
+            "id": faq.id,
+            "question": translated_data["question"],
+            "answer": translated_data["answer"],
+        })
+
+    # Use the FAQSerializer to serialize the translated FAQ data
+    serializer = FAQSerializer(faq_data, many=True)
     return Response(serializer.data)
